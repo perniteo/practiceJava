c63f1d5c2b167abcb2f74ae44207515973b1c904
perniteo <perniteo1@gmail.com>
2025-12-12 13:37:45 +0900

    auto: solved PCCP

---
+++ src/BaekAlgo/mergeSort/수_정렬하기2_2751.java
@@ -0,0 +1,2 @@
+package BaekAlgo.mergeSort;public class 수_정렬하기2_2751 {
+}
---
+++ src/Programmers/auto_push.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ROOT="/c/Users/perni/git/practice/src/Programmers"
+
+echo "Monitoring $ROOT for DONE files..."
+
+while true; do
+    DONE_FILES=$(find "$ROOT" -name "DONE" 2>/dev/null)
+
+    for FILE in $DONE_FILES; do
+        DIR=$(dirname "$FILE")
+        FOLDER_NAME=$(basename "$DIR")
+
+        echo "[FOUND] DONE in: $DIR"
+
+        git -C "$ROOT" add .
+        git -C "$ROOT" commit -m "auto: solved $FOLDER_NAME"
+        git -C "$ROOT" push
+
+        rm "$FILE"
+        echo "[PUSHED] Uploaded & deleted DONE for $FOLDER_NAME"
+    done
+
+    sleep 3
+done
